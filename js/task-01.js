0ed9dff7445d80907002cb7139c118e756e3c0f8
LytvynNata <96059882+LytvynNata@users.noreply.github.com>
2023-10-20 10:08:58 +0200

    task 1 corr

--- js/task-01.js
+++ js/task-01.js
@@ -1,11 +1,7 @@
-const categories = document.querySelector("#categories");
+const allCategories = document.querySelectorAll(".item");
+console.log("Number of categories: " + allCategories.length);
 
-const numberOfCategories = categories.querySelectorAll("li.item").length;
-console.log(`Number of categories: ${numberOfCategories}`);
-
-categories.querySelectorAll("li.item").forEach((category) => {
-  const title = category.querySelector("h2").textContent;
-  const numberOfItems = category.querySelectorAll("li").length;
-
-  console.log(`Category: ${title}, Elements: ${numberOfItems}`);
+allCategories.forEach((elem) => {
+  console.log("Category: " + elem.firstElementChild.textContent);
+  console.log("Elements: " + elem.lastElementChild.children.length);
 });
